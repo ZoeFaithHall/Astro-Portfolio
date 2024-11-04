29de935b286a43862958dc679dbd3b042cdd3912
Zoë Hall <116403413+ZoeFaithHall@users.noreply.github.com>
2024-11-04 09:17:10 -0500

    Speed Insights and Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
-  site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), ]
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  integrations: [tailwind(), sitemap()],
 });
